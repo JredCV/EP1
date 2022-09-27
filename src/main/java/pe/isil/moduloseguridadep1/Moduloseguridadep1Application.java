78a38d1b149e9b65d9446c88c72b923d2ac5c27c
JredCV <cvjared14@gmail.com>
2022-09-27 03:00:22 -0500

    Primero commit

---
+++ src/main/java/pe/isil/moduloseguridadep1/Moduloseguridadep1Application.java
@@ -0,0 +1,16 @@
+package pe.isil.moduloseguridadep1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Moduloseguridadep1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Moduloseguridadep1Application.class, args);
+
+		System.out.println("Hola mundo");
+	}
+
+}
---
+++ src/test/java/pe/isil/moduloseguridadep1/Moduloseguridadep1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.isil.moduloseguridadep1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Moduloseguridadep1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
